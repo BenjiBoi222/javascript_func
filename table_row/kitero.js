081127a989eb6ad38bacbfa05003a97f4ea7ce13
BenjiBoi222 <schlaffer.karoly_2022b@bolyai-szakkozep.hu>
2025-10-10 08:55:39 +0200

    bleh

---
+++ table_row/kitero.js
@@ -0,0 +1,33 @@
+const object = {  }
+if(object > 5){
+    object.name = "Név" 
+}else{
+    object.name = "Feri"
+}
+
+console.log(object)
+console.log(object["name"])
+
+
+if (object.age == undefined){
+    null
+}
+
+if (object.age == null) {}
+
+if (object.age === null){}
+
+object.age = null;
+
+if (object.age){}
+
+
+let val = 6;
+if (val == "6") console.log(true);
+else console.log(false);
+
+if(val === "6") console.log(true);
+else console.log(false);
+
+
+
